c8c6e00e7c7e08458a75048156faeba322509141
Mateusz Prasal <mateusz.prasal@gmail.com>
2020-09-29 15:10:53 +0200

    iOS 14 support

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
 declare module 'react-native-idfa' {
   export namespace IDFA {
     export function getIDFA(): Promise<string>;
+    export function requestTrackingPermission(): Promise<string>
   }
 }
